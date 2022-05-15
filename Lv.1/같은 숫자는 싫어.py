6b48f9ce8709421eab343db61059f8f93573aa1a
mina <m1nna@naver.com>
2022-05-15 17:04:55 +0900

    같은 숫자는 싫어

--- Lv.1/같은 숫자는 싫어.py
+++ Lv.1/같은 숫자는 싫어.py
@@ -33,24 +33,50 @@ arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
 
 # 질문을 통해) 기존 배열의 원소를 빼는 것보다, 새 배열에 추가하는 방식이 더 빠르다는 것을 알게됨.
 # 삭제 -> 추가로 코드를 바꿔봤지만, 효율성 테스트 4개중 3개만 성공
-# def solution(arr):
-#     i = 0
-#     answer = []
-#     while i < len(arr) - 1:
-#         if arr[i-1] != arr[i] != arr[i+1]:
-#             answer.append(arr[i])
-#             i += 1
-#         elif arr[i] == arr[i+1]:
-#             if len(answer) > 0 and answer[-1] == arr[i]:
-#                 i += 1
-#             else:
-#                 answer.append(arr[i])
-#                 i += 1
-#         else:
-#             i += 1
-
-#     return answer
 
 
-# print(solution([1, 1, 3, 3, 0, 1, 1]))
-# print(solution([4, 4, 4, 3, 3]))
+def solution(arr):
+    i = 0
+    answer = []
+    while i < len(arr) - 1:
+        if arr[i-1] != arr[i] != arr[i+1]:
+            answer.append(arr[i])
+            i += 1
+        elif arr[i] == arr[i+1]:
+            if len(answer) > 0 and answer[-1] == arr[i]:
+                i += 1
+            else:
+                answer.append(arr[i])
+                i += 1
+        else:
+            i += 1
+
+    return answer
+
+
+print(solution([1, 1, 3, 3, 0, 1, 1]))
+print(solution([4, 4, 4, 3, 3]))
+
+
+# 다른 사람 풀이)
+def solution1(arr):
+    answer = []
+    for i in arr:
+        if answer[-1:] == [i]:
+            continue
+        answer.append(i)
+    return answer
+
+# 배운점: answer[-1]을 통해 비교하면 empty array일때 인덱싱오류가남.
+# [-1:]를 통해 리스트를 만들어 비교하기때문에, i에 []를 통해 리스트의 형태를 갖춰야 함.
+
+
+def solution2(arr):
+    answer = []
+    answer.append(arr[0])
+
+    for i in range(1, len(arr)):
+        if arr[i-1] != arr[i]:
+            answer.append(arr[i])
+
+    return answer

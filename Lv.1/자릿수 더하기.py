81760b886ae4a5edcd7df9782ba4b0149940840c
mina <m1nna@naver.com>
2022-05-17 23:32:38 +0900

    자릿수 더하기

---
+++ Lv.1/자릿수 더하기.py
@@ -0,0 +1,28 @@
+
+from re import S
+
+# 문제 설명
+"""
+자연수 N이 주어지면, 
+N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+"""
+
+# 풀이 생각중)
+
+
+def solution(n):
+    answer = 0
+    while True:
+        s = n % 10
+        k = n // 10
+        t = k % 10
+        j = k // 10
+        print(f"s={s}")
+        print(f"k={k}")
+        print(f"t={t}")
+        print(f"j={j}")
+        break
+
+
+solution(123)

df7e400888f5566fc8227675b831940d333dce11
mina <m1nna@naver.com>
2022-06-14 15:26:38 +0900

    소수 만들기

--- Lv.1/소수 만들기.py
+++ Lv.1/소수 만들기.py
@@ -6,19 +6,27 @@ nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때
 소수가 되는 경우의 개수를 return 하도록 solution 함수를 완성해주세요.
 """
 
-import random
-
-
 # 풀이 생각 중
+import itertools
+
+
 def solution(nums):
-    answer = 0
-    num = sum(random.sample(nums, 3))
-    print(num)
-    if num & 2 != 0 | num & 3 != 0:
-        answer += 1
+    answer = []
     # 3개씩 선정
-    # 소수 분별
-    return answer
+    result = list(itertools.permutations(nums, 3))
+    print(result)
+    for i in result:
+        x = sum(i)
+        print(x)
+        if x == 1 or x == 2 or x == 3:
+            if x not in answer:
+                answer.append(x)
+        elif x % 2 != 0 | x % 3 != 0:
+            if x not in answer:
+                answer.append(x)
+    print(answer)
+    print(f"확인 중: {8 % 2}")
+    return len(set(answer))
 
 
 print(solution([1, 2, 3, 4]))
